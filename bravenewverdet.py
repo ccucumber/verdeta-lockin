7c8a7e73c7249026981f9937d0e97ceadec50f09
Michał Włodarczyk <mwlodarczyk0@gmail.com>
2018-12-07 19:32:14 +0100

    First analog lockin MAJOR FUCKUP NO SYNC BIT

--- bravenewverdet.py
+++ bravenewverdet.py
@@ -42,16 +42,19 @@ def meas_diff():
     potentiostat.io_clear(3)
     return result
 
-def meas_lockin_d(kom):
+def meas_lockin_a(kom,t):
+    kom.write_command_stdr("VOLT 0 200 3500", 249)
     kom.data=[]
     kom.write_command_stdr("START", 249)
-    time.sleep(2.2)
+    time.sleep(t)
     kom.write_command_stdr("STOP", 249)
     time.sleep(.1)
     trace.info("Data in buffer: "+str(len(kom.data)))
     first=None
     pha=0
-    s = np.array(kom.data)[:, (0, 3)]
+    s = np.array(kom.data)[:, (0, 2)]
+
+
     start=0
     mid=0
     stop=0
@@ -89,6 +92,70 @@ def meas_lockin_d(kom):
     mult=2*(s[:stop,1]-0.5)
     sig=s[:stop,0]
 
+    # save data to text file
+
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+
+    np.savetxt('probe_' + str(osc_count) + " "+ timestamp + '.txt',  np.c_[sig,mult,s[:stop,1]], header='U[V] Status')
+
+    return np.dot(sig,mult)/stop
+
+
+def meas_lockin_d(kom,t):
+    kom.write_command_stdr("LOCKIN 60 3", 250)
+    kom.data=[]
+    kom.write_command_stdr("START", 249)
+    time.sleep(t)
+    kom.write_command_stdr("STOP", 249)
+    time.sleep(.1)
+    trace.info("Data in buffer: "+str(len(kom.data)))
+    first=None
+    pha=0
+    s = np.array(kom.data)[:, (0, 3)]
+
+
+    start=0
+    mid=0
+    stop=0
+    osc_count=0
+    for i in range(len(s)):
+        if(first is None): first=s[i,1]
+        else:
+            if s[i,1] != first:
+                start=i
+                mid=i
+                stop=i
+                break
+
+    s=s[start:]
+    start=0
+    first = s[0, 1]
+    trace.debug("Start index: "+str(start))
+
+    for i in range(len(s)):
+        if s[i, 1] != first:
+            first=s[i,1]
+            pha+=1
+            if pha==2:
+                pha=0
+                if mid-start == i-mid:
+                    start=i
+                    stop=i
+                    osc_count+=1
+                    trace.debug("Found osc prd="+str(start-mid))
+                else:
+                    trace.error("Inconsistent prd: "+str(start)+" "+str(mid)+" "+str(stop))
+            mid = i
+    trace.info("Oscillations: "+str(osc_count)+" Samples left: "+str(len(s)-stop))
+
+    mult=2*(s[:stop,1]-0.5)
+    sig=s[:stop,0]
+
+    # save data to text file
+
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+
+    np.savetxt('probe_' + str(osc_count) + " "+ timestamp + '.txt',  np.c_[sig,mult,s[:stop,1]], header='U[V] Status')
 
     return np.dot(sig,mult)/stop
 
@@ -97,7 +164,7 @@ def meas_lockin_d(kom):
 if __name__ == "__main__":
     trace.basicConfig(format="[%(asctime)s](%(module)s:%(funcName)s:%(lineno)d) %(message)s",
                       datefmt='%I:%M:%S',
-                      level=trace.INFO)
+                      level=trace.DEBUG)
     kom = fotonowy.Communication()
     kom.connect( port_name="/dev/ttyUSB0")
     monochromator=fotonowy.Monochromator(kom)
@@ -106,11 +173,12 @@ if __name__ == "__main__":
 
     monochromator.shutter_open()
     print("Range "+ str(potentiostat.set_range(2)))
-    potentiostat.set_avg(20)
+    potentiostat.set_avg(10)
     potentiostat.set_freq(1000)
-    potentiostat.set_volt(2)#,10,3.5)
+    kom.write_command_stdr("CEOFF 3", 249)
+    #potentiostat.set_volt(0,10,3.5)
 
 
     for i in range(10):
-        kom.write_command_stdr("LOCKIN 200 0", 250)
-        print(meas_lockin_d(kom))
+        #potentiostat.set_volt(i/2.5)
+        print(meas_lockin_a(kom,0.5))
