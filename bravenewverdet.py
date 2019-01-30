b337c7361c5eb03f7ae8c530e42919124eb67462
Michał Włodarczyk <mwlodarczyk0@gmail.com>
2019-01-30 18:26:31 +0100

    Last test version

--- bravenewverdet.py
+++ bravenewverdet.py
@@ -14,11 +14,23 @@ from matplotlib.backend_bases import key_press_handler
 
 from matplotlib.figure import Figure
 
+import matplotlib
+matplotlib.use('TkAgg')
+
+from numpy import arange, sin, pi
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+# implement the default mpl key bindings
+from matplotlib.backend_bases import key_press_handler
+
+
+from matplotlib.figure import Figure
+
 import sys
 if sys.version_info[0] < 3:
     import Tkinter as Tk
 else:
     import tkinter as Tk
+
 
 
 def meas_ref():
@@ -42,129 +54,7 @@ def meas_diff():
     potentiostat.io_clear(3)
     return result
 
-def meas_lockin_a(kom,pot,t,amp,offs):
 
-
-    pot.set_freq(0)
-    kom.write_command_stdr("CEOFF 0", 249)
-    kom.data=[]
-    pot.set_volt(offs,15,amp)
-    kom.write_command_stdr("START", 249)
-    time.sleep(t)
-    kom.write_command_stdr("STOP", 249)
-    time.sleep(.1)
-    trace.info("Data in buffer: "+str(len(kom.data)))
-    first=None
-    pha=0
-    s = np.array(kom.data)[:, (0, 2)]
-
-
-    start=0
-    mid=0
-    stop=0
-    osc_count=0
-    for i in range(len(s)):
-        if(first is None): first=s[i,1]
-        else:
-            if s[i,1] != first:
-                start=i
-                mid=i
-                stop=i
-                break
-
-    s=s[start:]
-
-    first = s[0, 1]
-    trace.info("Start index: "+str(start))
-    start = 0
-
-    for i in range(len(s)):
-        if s[i, 1] != first:
-            first=s[i,1]
-            pha+=1
-            if pha==2:
-                pha=0
-                if mid-start == i-mid:
-                    start=i
-                    stop=i
-                    osc_count+=1
-                    trace.info("Found osc prd="+str(start-mid))
-                else:
-                    trace.error("Inconsistent prd: "+str(start)+" "+str(mid)+" "+str(stop))
-            mid = i
-    trace.info("Oscillations: "+str(osc_count)+" Samples left: "+str(len(s)-stop))
-    #stop=len(s)
-    mult=2*(s[:stop,1]-0.5)
-    sig=s[:stop,0]
-
-    # save data to text file
-
-    timestamp = time.strftime("%Y%m%d-%H%M%S")
-
-    #np.savetxt('probe_' + str(osc_count) + "_"+ timestamp + '.txt',  np.c_[sig,mult,s[:stop,1]], header='U[V] Status')
-    np.savetxt('analog.txt', np.c_[sig, mult, s[:stop, 1]],
-               header='U[V] Status')
-
-    return np.dot(sig,mult)/stop
-
-
-def meas_lockin_d(kom,t):
-    kom.write_command_stdr("LOCKIN 60 3", 250)
-    kom.data=[]
-    kom.write_command_stdr("START", 249)
-    time.sleep(t)
-    kom.write_command_stdr("STOP", 249)
-    time.sleep(.1)
-    trace.info("Data in buffer: "+str(len(kom.data)))
-    first=None
-    pha=0
-    s = np.array(kom.data)[:, (0, 3)]
-
-
-    start=0
-    mid=0
-    stop=0
-    osc_count=0
-    for i in range(len(s)):
-        if(first is None): first=s[i,1]
-        else:
-            if s[i,1] != first:
-                start=i
-                mid=i
-                stop=i
-                break
-
-    s=s[start:]
-    start=0
-    first = s[0, 1]
-    trace.debug("Start index: "+str(start))
-
-    for i in range(len(s)):
-        if s[i, 1] != first:
-            first=s[i,1]
-            pha+=1
-            if pha==2:
-                pha=0
-                if mid-start == i-mid:
-                    start=i
-                    stop=i
-                    osc_count+=1
-                    trace.debug("Found osc prd="+str(start-mid))
-                else:
-                    trace.error("Inconsistent prd: "+str(start)+" "+str(mid)+" "+str(stop))
-            mid = i
-    trace.info("Oscillations: "+str(osc_count)+" Samples left: "+str(len(s)-stop))
-
-    mult=2*(s[:stop,1]-0.5)
-    sig=s[:stop,0]
-
-    # save data to text file
-
-    timestamp = time.strftime("%Y%m%d-%H%M%S")
-
-    np.savetxt('probe_' + str(osc_count) + " "+ timestamp + '.txt',  np.c_[sig,mult,s[:stop,1]], header='U[V] Status')
-
-    return np.dot(sig,mult)/stop
 
 
 
@@ -172,6 +62,14 @@ if __name__ == "__main__":
     trace.basicConfig(format="[%(asctime)s](%(module)s:%(funcName)s:%(lineno)d) %(message)s",
                       datefmt='%I:%M:%S',
                       level=trace.INFO)
+
+    root = Tk.Tk()
+    root.wm_title("Embedding in TK")
+
+    f = Figure(figsize=(5, 4), dpi=100)
+    a = f.add_subplot(111)
+
+
     kom = fotonowy.Communication()
     kom.connect( port_name="/dev/ttyUSB0")
     monochromator=fotonowy.Monochromator(kom)
@@ -179,15 +77,62 @@ if __name__ == "__main__":
 
 
     monochromator.shutter_open()
-    print("Range "+ str(potentiostat.set_range(2)))
+    print("Range "+ str(potentiostat.set_range(4)))
     potentiostat.set_avg(1)
-    potentiostat.set_freq(0)
+    #potentiostat.set_freq(0)
     #kom.write_command_stdr("CEOFF 3", 249)
-    #potentiostat.set_volt(0,55,3.5)
 
 
-    for i in range(1,10):
+    canvas = FigureCanvasTkAgg(f, master=root)
+    canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
+    canvas._tkcanvas.pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
 
-        print(meas_lockin_a(kom,potentiostat,1.9,2,i/2.5))
+    toolbar = NavigationToolbar2TkAgg(canvas, root)
+    toolbar.update()
+
+    X=np.arange(-1,1,.1)
+    Y=np.zeros_like(X)
+
+    plt, = a.plot(X, Y)
+    canvas.show()
+
+    for i, x in enumerate(X):
+
+        potentiostat.set_volt(x, 51, 1)
+        Y[i] = potentiostat.meas_lockin_a(1)
+        print(Y[i])
+        plt.set_ydata(Y)
+
+        a.relim()
+        a.autoscale_view(True, True, True)
+        canvas.draw()
+
+
+
+    # a tk.DrawingArea
+
+
+
+    def on_key_event(event):
+        print('you pressed %s' % event.key)
+        key_press_handler(event, canvas, toolbar)
+
+
+    canvas.mpl_connect('key_press_event', on_key_event)
+
+
+    def _quit():
+        root.quit()  # stops mainloop
+        root.destroy()  # this is necessary on Windows to prevent
+        # Fatal Python Error: PyEval_RestoreThread: NULL tstate
+
+
+    button = Tk.Button(master=root, text='Quit', command=_quit)
+    button.pack(side=Tk.BOTTOM)
+
+    Tk.mainloop()
+    # If you put root.destroy() here, it will cause an error if
+    # the window is closed with the window manager.
+
 
     kom.disconnect()
